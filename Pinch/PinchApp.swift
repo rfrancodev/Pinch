db195acf309d648220a9f6039f8b76c52e7ecfa3
rfrancodev <francorfs@icloud.com>
2024-10-04 15:28:20 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Rafael Franco on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
