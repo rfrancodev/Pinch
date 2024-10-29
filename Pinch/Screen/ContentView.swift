23c91fe6c8d921293f62fce8e36a8f8fc2925fc3
rfrancodev <francorfs@icloud.com>
2024-10-28 21:03:26 -0300

    Gesto de deslizar, zoom e reset posiçao

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -11,10 +11,17 @@ struct ContentView: View {
     // MARK: - PROPETIES
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
-    @State private var imageOffset: CGPoint = .zero
+    @State private var imageOffset: CGSize = .zero
     
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+        imageScale = 1
+        imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -29,7 +36,9 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                
                 // MARK: - TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
@@ -37,11 +46,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                //MARK: - DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                        )
                 
             } //: ZSTACK
             .navigationTitle("Pinch && Zoom")
