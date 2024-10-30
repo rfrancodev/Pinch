472823b3721deeeefe7cf88ce59306763043156b
rfrancodev <francorfs@icloud.com>
2024-10-30 11:09:32 -0300

    Adicionado controles de aumentar e diminuir imagem

---
+++ Pinch/View/ControlImageView.swift
@@ -0,0 +1,24 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Rafael Franco on 30/10/24.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+#Preview {
+    ControlImageView(icon: "minus.magnifyingglass")
+        .preferredColorScheme(.dark)
+        .padding()
+}
+
